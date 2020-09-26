bcb24577c5d803bb2beb939c1ac5fbb31ffd50ab
eunha0ne <eunha0ne@gmail.com>
2020-09-26 20:07:45 +0900

    Feat: 헤더 컴포넌트를 작성하라

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,27 @@
+import React, { useEffect, useState } from "react";
+import { authService } from "app/firebaseInstance";
+
+import useLoggedInState from "hooks/useLoggedInState";
+
+export default () => {
+  const isLoggedIn = useLoggedInState();
+  const [userName, setUserName] = useState(null);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      const { displayName, email } = authService.currentUser;
       setUserName(displayName || email.split("@")[0]);
+    }
+  }, [isLoggedIn]);
+
+  return (
+    <header>
+      <h1>FireBase App</h1>
+      {userName && (
+        <p>
+          환영합니다! <strong>{userName}</strong>
+        </p>
+      )}
+    </header>
+  );
+};
