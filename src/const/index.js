d5187b0d6de4874ce29acf6683428ca9cfc32fea
eunha0ne <eunha0ne@gmail.com>
2020-09-27 03:26:36 +0900

    Feat: 검색 api를 추가하라

---
+++ src/const/index.js
@@ -0,0 +1,11 @@
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
+export const KAKAO_API_KEY = process.env.REACT_APP_KAKAO_API_KEY;
+export const FIREBASE_CONFIG = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID
+};
