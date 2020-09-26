9b3e43eca3955606c1b6084219adfb8e84338b2e
eunha0ne <eunha0ne@gmail.com>
2020-09-26 16:26:31 +0900

    Feat: 모달 닫기 기능을 추가하라

--- src/features/Modal.js
+++ src/features/Modal.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+
+import { hideModal } from "features/modalSlice";
 
 export default ({ data }) => {
+  const dispatch = useDispatch();
   const { message } = data;
+
+  const onClose = () => {
+    dispatch(hideModal());
+  };
 
   return (
     <article>
       <p>{message}</p>
+      <button onClick={onClose}>닫기</button>
     </article>
   );
 };
