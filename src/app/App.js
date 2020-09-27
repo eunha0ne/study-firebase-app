9034ef407deaf5754cc33f57d0e84e11780134db
eunha0ne <eunha0ne@gmail.com>
2020-09-27 21:22:33 +0900

    Feat: `styled-compoents`를 적용하라

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,39 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { HashRouter as Router } from "react-router-dom";
+import styled, { createGlobalStyle } from "styled-components";
 
 import AppRouter from "./AppRouter";
 import Modal from "features/Modal";
 import Header from "components/Header";
 import Footer from "components/Footer";
+
+const GlobalStyle = createGlobalStyle`  
+  #root {
+    margin: 0 auto;
+    max-width: 1024px;
+  }
+  
+  ul,
+  li {
+    margin: 0;
+    padding: 0;
+  }
+  
+  ul {
+    list-style: none;
+  }
+  
+  p {
+    margin: 0;
+  }
+`;
+
+const S = {
+  Main: styled.main`
+    border: 1px solid red;
+  `
+};
 
 function App() {
   const { modalState } = useSelector((state) => ({
@@ -15,11 +43,12 @@ function App() {
   return (
     <Router>
       <Header />
-      <main>
+      <S.Main>
         <AppRouter />
         {modalState.isOpen && <Modal {...modalState} />}
-      </main>
+      </S.Main>
       <Footer />
+      <GlobalStyle />
     </Router>
   );
 }
